1e13a20a904b34a9b22d70b4cf71f046fc36595f
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-10-15 23:30:07 +0500

    Добавил environment

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -1,4 +1,7 @@
-const secret = 'some-secret-key';
+const { NODE_ENV } = process.env;
+const { JWT_SECRET } = process.env;
+
+const secret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
 
 const urlRegExp = /(https?:\/\/)(w{3}\.)?([a-zA-Z0-9-]{0,63}\.)([a-zA-Z]{2,4})(\/[\w\-._~:/?#[\]@!$&'()*+,;=]#?)?/;
 
