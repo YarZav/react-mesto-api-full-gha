6599f6de684eec2753880f44bb957df2ca82271d
Iaroslav Zavialov <yaroslavzavyalov1@gmail.com>
2023-10-15 17:52:23 +0500

    Добавил логирование запросов и ошибок

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const helmet = require('helmet');
 const { errors } = require('celebrate');
 const routes = require('./routes/routes');
 const errorHandler = require('./errors/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -13,7 +14,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
+app.use(requestLogger);
 app.use('/', routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
